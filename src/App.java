d887b9cc70d9960f826309ae298de883539c6a41
Bucccket <37452311+bucccket@users.noreply.github.com>
2023-10-08 23:12:39 +0200

    Amended text to test Git connectivity

--- src/App.java
+++ src/App.java
@@ -3,5 +3,6 @@ public class App {
 	public static void main(String[] argv) {
 		System.out.println("Hello Java!");
 		System.out.println("Hi im orange!");
+		System.out.println("That is so cool!");
 	}
 }
