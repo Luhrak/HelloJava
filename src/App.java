8ce1c4b324cb297c20e1b2aefb81663ca1aa379b
Luhrak <lukass@fn.de>
2023-10-08 23:15:18 +0200

    flop

--- src/App.java
+++ src/App.java
@@ -4,5 +4,6 @@ public class App {
 		System.out.println("Hello Java!");
 		System.out.println("Hi im orange!");
 		System.out.println("That is so cool!");
+		System.out.println("Ikr, can I get a name to your order?!");
 	}
 }
